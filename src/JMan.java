31ff8084b9342855cb812b3b30b50938eec8073c
GamTS <Spirit_windwalk@hotmail.com>
2016-05-12 00:04:06 +0700

    jman finished

--- src/JMan.java
+++ src/JMan.java
@@ -40,11 +40,73 @@ public class JMan extends Piece {
         0 = up, 1 = down, 2 = left, 3 = right. */
     public void step(int i){
         setActed(true);
-        
+        	
         // Complete this
-        if(i == 0)
+        if(i == 0){
+        	if(getMap().isInGrid(getX(),getY() - 1)){
+        		Piece pi = getMap().pieceAt(getX(),getY() - 1);
+        		if(pi == null){
+        			getMap().move(getX(), getY(), getX(), getY() - 1);
+                    setY(getY() - 1);
+        		}
+        		else if(pi.getType() != 0 && eatit(pi)) {
+                    setColor(pi.getColor());
+                    getMap().move(getX(), getY(), getX(), getY() - 1);
+                    setY(this.getY() - 1);
+                }
+        	}
+        }else if(i == 1) {
+            if(getMap().isInGrid(getX(), getY() + 1)) {
+            	Piece pi = getMap().pieceAt(getX(), getY() + 1);
+                if(pi == null) {
+                    getMap().move(getX(), getY(), getX(), getY() + 1);
+                    setY(getY() + 1);
+                } else if(pi.getType() != 0 && this.eatit(pi)) {
+                    setColor(pi.getColor());
+                    getMap().move(getX(), getY(), getX(), getY() + 1);
+                    setY(getY() + 1);
+                }
+            }
+        } else if(i == 2) {
+            if(getMap().isInGrid(getX() - 1, getY())) {
+            	Piece pi = getMap().pieceAt(getX() - 1, getY());
+                if(pi == null) {
+                    getMap().move(getX(), getY(), getX() - 1, getY());
+                    setX(getX() - 1);
+                } else if(pi.getType() != 0 && eatit(pi)) {
+                    setColor(pi.getColor());
+                    getMap().move(getX(), getY(), getX() - 1, getY());
+                    setX(getX() - 1);
+                }
+            }
+        } else if(getMap().isInGrid(getX() + 1, getY())) {
+        	Piece pi = getMap().pieceAt(getX() + 1, getY());
+            if(pi == null) {
+                getMap().move(getX(), getY(), getX() + 1, getY());
+                setX(this.getX() + 1);
+            } else if(pi.getType() != 0 && eatit(pi)) {
+                setColor(pi.getColor());
+                getMap().move(getX(), getY(), getX() + 1, getY());
+                setX(getX() + 1);
+            }
+        }
+
+        	
         	
         
+    }
+    
+    public boolean eatit(Piece pi){
+    	
+    		if(getColor() == Color.RED && pi.getColor() == Color.YELLOW )
+    			return true;
+    		else if (getColor() == Color.GREEN && pi.getColor() == Color.RED )
+    			return true;
+    		else if (getColor() == Color.YELLOW && pi.getColor() == Color.GREEN)
+    			return true;
+    		else
+    			return false;
+    	  
     }
     
     /** = representation of this piece */
